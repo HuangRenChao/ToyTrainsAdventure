b26db1a98228d3e76f37972cc1968578697095e4
Hyunghwan <bhhwan@gmail.com>
2016-04-19 23:02:21 -0700

    Test gihub - unity

---
+++ Assets/Scripts/CubeMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMove : MonoBehaviour {
+
+	public float movingSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += Vector3.right * movingSpeed * Time.deltaTime;
+	}
+}
